b67c1b091d963998f8da2b599bf9206fc861b250
Olga Maksymova <maximova.olga@gmail.com>
2017-03-17 16:32:23 +0200

    Responds+tech consultation

--- build/php/send.php
+++ build/php/send.php
@@ -1,8 +1,10 @@
-<?
-if((isset($_POST['z_name'])&&$_POST['z_name']!="")&&(isset($_POST['z_tel'])&&$_POST['z_tel']!="")) { 
+<?php
+if((isset($_POST['z_name'])&&$_POST['z_name']!="")&&(isset($_POST['z_tel'])&&$_POST['z_tel']!="")&&(isset($_POST['z_email'])&&$_POST['z_email']!="")) { 
 //Проверка отправилось ли наше поля name и не пустые ли они
-        $to = 'maximova.olga@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
-        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $to = 'maximova.olga@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно
+        if ( isset($_POST['z_checkbox'])&&$_POST['z_checkbox']=='on' ) {$help = 'Нужна консультация специалиста';}
+        else $help = 'Консультация специалиста не нужна';
+        $subject = 'ЗАКАЗ'; //Загаловок сообщения
         $message = '
                 <html>
                     <head>
@@ -10,13 +12,19 @@ if((isset($_POST['z_name'])&&$_POST['z_name']!="")&&(isset($_POST['z_tel'])&&$_P
                     </head>
                     <body>
                         <p>Имя: '.$_POST['z_name'].'</p>
-                        <p>Телефон: '.$_POST['z_tel'].'</p>                        
+                        <p>Телефон: '.$_POST['z_tel'].'</p>
+                        <p>Email: '.$_POST['z_email'].'</p>
+                        <p>Примечания: '.$_POST['z_text'].'</p>
+                        <p>'.$help.'</p>                             
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
-        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+        $headers .= "From: Заказ на стеллаж <callback@gir.ua>\r\n"; //Наименование и почта отправителя
+        if (mail($to, $subject, $message, $headers)) //Отправка письма с помощью функции mail
+        	echo "Ваше сообщение отправлено!";
+        else
+        	echo "Не удалось отправить сообщение!";
+} else {
+	echo "Не все поля заполнены!";
 }
-
-
 ?>
